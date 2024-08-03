97260dd9471ca983e632f7ebfac14f252e43bcc9
eveffer <eliveffer@gmail.com>
2024-08-03 17:50:14 +0300

    checkpoint

--- mod.ts
+++ mod.ts
@@ -1,8 +1,4 @@
-export { EasyApi } from "#/client/easyApi.ts";
-
 export { createAction } from "#/actions/createAction.ts";
 
 export type { EasyResponse } from "#/easyResponse.ts";
 export type { EasyRequest } from "#/easyRequest.ts";
-
-export { EasyApp } from "#/easyApp.ts";
