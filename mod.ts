4f852a7bb48ec5ba54bf78f701ad1989f99fe4e8
eveffer <67253604+eveffer@users.noreply.github.com>
2024-08-11 15:49:55 +0300

    Export easypackage

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 export { EasyException, raiseEasyException } from "#/easyException.ts";
-
+export { EasyPackage } from "#/package/easyPackage.ts";
 export { EasyApp } from "#/easyApp.ts";
 
 export { createAction } from "#/actions/createAction.ts";
