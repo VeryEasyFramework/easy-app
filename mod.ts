540acf1855bd3ef24120cadf6a376614998d10a6
eveffer <eliveffer@gmail.com>
2024-08-13 14:51:11 +0300

    export easy log

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
+export { easyLog } from "#/log/logging.ts";
+
 export { EasyException, raiseEasyException } from "#/easyException.ts";
 export { EasyPack } from "./src/package/easyPack.ts";
 export { EasyApp } from "#/easyApp.ts";
-
 export { createAction } from "#/actions/createAction.ts";
 
 export type { EasyResponse } from "#/easyResponse.ts";
