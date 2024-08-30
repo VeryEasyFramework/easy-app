e9a130963c34bd9b38473b1fab593243e9ac27aa
eveffer <67253604+eveffer@users.noreply.github.com>
2024-08-30 11:42:22 +0300

    checkpoint

--- src/package/authPackage/authPack.ts
+++ src/package/authPackage/authPack.ts
@@ -1,13 +1,14 @@
 import { EasyPack } from "#/package/easyPack.ts";
 import { userEntity } from "#/package/authPackage/entities/userEntity.ts";
 import { authMiddleware } from "#/package/authPackage/middleware/authMiddleware.ts";
+import { userSessionEntity } from "#/package/authPackage/entities/userSession.ts";
 
 const authPack = new EasyPack("auth", {
   description: "Package for authentication",
 });
 
 authPack.addEntity(userEntity);
-
+authPack.addEntity(userSessionEntity);
 // authPackage.addMiddleware(authMiddleware);
 
 export { authPack };
