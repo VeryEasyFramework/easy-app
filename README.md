16fd9909e1169df50a7c9fd70a5ddec172614610
eveffer <eliveffer@gmail.com>
2024-09-20 04:56:39 +0300

    checkpoint

--- README.md
+++ README.md
@@ -2,4 +2,22 @@
 
 A super easy web app builder for your Deno project
 
+## Quick Start
+
+1. Add the package to your project
+
+```shell
+deno add @vef/easy-app
+```
+
+2. Import and run EasyApp
+
+```ts
+import { EasyApp } from "@vef/easy-app";
+// Create a new instance of EasyApp
+const app = new EasyApp();
+// Run the app
+app.run();
+```
+
 Documentation at [VeryEasyFramework](https://veryeasyframework.com)
--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -2,12 +2,27 @@ import type { EasyRequest } from "#/easyRequest.ts";
 import type { EasyResponse } from "#/easyResponse.ts";
 import type { EasyApp } from "#/easyApp.ts";
 
+/**
+ * Middleware function type
+ */
 export type MiddleWare = (
+  /**
+   * EasyApp instance
+   */
   app: EasyApp,
+  /**
+   * EasyRequest instance
+   */
   request: EasyRequest,
+  /**
+   * EasyResponse instance
+   */
   response: EasyResponse,
 ) => Promise<void> | void;
 
+/**
+ * function to create a middleware for the app
+ */
 export function createMiddleware(
   middleware: MiddleWare,
 ): MiddleWare {
